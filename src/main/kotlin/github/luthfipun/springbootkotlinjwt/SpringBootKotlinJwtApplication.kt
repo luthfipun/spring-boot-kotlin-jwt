a6568acaea10342cf14f262673f618a08a2646e5
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-09-13 19:10:03 +0700

    init

---
+++ src/main/kotlin/github/luthfipun/springbootkotlinjwt/SpringBootKotlinJwtApplication.kt
@@ -0,0 +1,11 @@
+package github.luthfipun.springbootkotlinjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinJwtApplication>(*args)
+}
---
+++ src/test/kotlin/github/luthfipun/springbootkotlinjwt/SpringBootKotlinJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.luthfipun.springbootkotlinjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
